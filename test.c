93b60ada83e60f954e181b15d090db9df6165701
Fadi <fadih1992@gmail.com>
2022-03-19 19:58:13 -0700

    test

--- test.c
+++ test.c
@@ -16,7 +16,7 @@ int startx = 0;
 int starty = 0;
 
 struct class_obj {
-    char symbol[20];
+    char symbol;
     int x;
     int y;
     int direction;
@@ -40,20 +40,21 @@ struct class_obj player = {};
 struct class_obj bullet = {};
 struct class_obj enemies[5] = {};
 
-void obj_init(struct class_obj *obj, int x, int y, int dir, char *objname) {
+void obj_init(struct class_obj *obj, int x, int y, int dir, char obj_shape) {
     obj->x = x;
     obj->y = y;
     obj->direction = dir;
-    strcpy(obj->symbol, objname);
+    obj->symbol = obj_shape;
+//    strcpy(obj->symbol, objname);
 }
 
-void eneymies_init(struct class_obj obj[], int x, int y, int dir, char *objname) {
+void eneymies_init(struct class_obj obj[], int x, int y, int dir, char obj_shape) {
     // obj = malloc(5 * sizeof(struct class_obj));
     for (int i = 0; i < 5; ++i) {
         obj[i].x = x;
         obj[i].y = y;
         obj[i].direction = dir;
-        strcpy(obj[i].symbol, objname);
+        obj[i].symbol = obj_shape;
     }
 }
 
@@ -65,11 +66,11 @@ int main() {
     pthread_t thread_1, thread_2;
     struct application_settings arg;
 
-    obj_init(&player, 5, 5, 0, "|O|");
+    obj_init(&player, 5, 5, 0, '@');
     // Bullet
-    obj_init(&bullet, player.x, player.x, 0, "*");
+    obj_init(&bullet, player.x, player.x, 0, '*');
 
-    eneymies_init(enemies, 50, 6, 0, "$");
+    eneymies_init(enemies, 50, 6, 0, '<');
 
     initscr();
     clear();
@@ -78,53 +79,53 @@ int main() {
     startx = 0;
     starty = 0;
 
+
     win = newwin(HEIGHT, WIDTH, starty, startx);
     keypad(win, TRUE);
     refresh();
-    //display_screen(win, Y, X, player);
-    display_enemies(win);
+    display_screen(win, Y, X, player);
 
-
-    if (pthread_create(&thread_1, NULL, &display_enemies, &win) != 0) {
+    if (pthread_create(&thread_1, NULL, &display_enemies, win) != 0) {
         perror("Failed to create thread");
     }
 
-//
-//    while (1) {
-//        c = wgetch(win);
-//        switch (c) {
-//            case KEY_UP:
-//                --Y;
-//                break;
-//            case KEY_DOWN:
-//                ++Y;
-//                break;
-//            case KEY_LEFT:
-//                --X;
-//                break;
-//            case KEY_RIGHT:
-//                ++X;
-//                break;
-//            case 's':
-//                arg.win = win;
-//                arg.X = X;
-//                arg.Y = Y;
-//                arg.ob = bullet;
-//                if (pthread_create(&thread_2, NULL, &display_bullet, &arg) != 0) {
-//                    perror("Failed to create thread");
-//                }
-//                break;
-//            default:
-//                refresh();
-//                break;
-//        }
-//        display_screen(win, Y, X, player);
-//        if (choice != 0)    /* User did a choice come out of the infinite loop */
-//            break;
-//    }
-//    clrtoeol();
-//    refresh();
-//    endwin();
+
+    while (1) {
+        c = wgetch(win);
+        mvwdelch(win, Y, X);
+        switch (c) {
+            case KEY_UP:
+                --Y;
+                break;
+            case KEY_DOWN:
+                ++Y;
+                break;
+            case KEY_LEFT:
+                --X;
+                break;
+            case KEY_RIGHT:
+                ++X;
+                break;
+            case 's':
+                arg.win = win;
+                arg.X = X;
+                arg.Y = Y;
+                arg.ob = bullet;
+                if (pthread_create(&thread_2, NULL, &display_bullet, &arg) != 0) {
+                    perror("Failed to create thread");
+                }
+                break;
+            default:
+                refresh();
+                break;
+        }
+        display_screen(win, Y, X, player);
+        if (choice != 0)    /* User did a choice come out of the infinite loop */
+            break;
+    }
+    clrtoeol();
+    refresh();
+    endwin();
     return 0;
 }
 
@@ -147,12 +148,17 @@ void *display_enemies(void *arg) {
             int rand = printRandoms(-1, 1);
             enemies[i].x--;
             enemies[i].y += rand;
-            mvwprintw(win_en, enemies[i].y, enemies[i].x, enemies[i].symbol);
+            mvwaddch(win_en, enemies[i].y, enemies[i].x, enemies[i].symbol);
             wrefresh(win_en);
         }
         nanosleep(&ts, NULL);
-        wclear(win_en);
-        wrefresh(win_en);
+        for (int i = 0; i < 5; ++i) {
+            mvwdelch(win_en, enemies[i].y, enemies[i].x);
+            wrefresh(win_en);
+        }
+        // mvwdelch(win_en, enemies[i].y, enemies[i].x);
+        //wclear(win_en);
+        //wrefresh(win_en);
     }
 }
 
@@ -169,7 +175,7 @@ void *display_bullet(void *arg) {
     for (int i = app_settings->X + 2; i < 35;) {
         i++;
         nanosleep(&ts, NULL);
-        mvwprintw(app_settings->win, y, i, app_settings->ob.symbol);
+        mvwaddch(app_settings->win, y, i, app_settings->ob.symbol);
         wrefresh(app_settings->win);
     }
     refresh();
@@ -178,10 +184,10 @@ void *display_bullet(void *arg) {
 
 
 void display_screen(WINDOW *win, int Y, int X, struct class_obj ob) {
-    refresh();
-    clear();
-    mvwprintw(win, Y, X, ob.symbol);
+//    refresh();
+//    clear();
+    //  mvwprintw(win, Y, X, ob.symbol);
 
-    // mvwaddch(win, X, Y, '*');
+    mvwaddch(win, Y, X, ob.symbol);
     wrefresh(win);
 }
