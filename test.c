4baa8a925b3d750868596c6441a9f363b8fa8977
Fadi <fadih1992@gmail.com>
2022-03-20 22:05:43 -0700

    first

--- test.c
+++ test.c
@@ -5,7 +5,6 @@
 #include <stdio.h>
 #include <ncurses.h>
 #include <string.h>
-#include <unistd.h>
 #include <pthread.h>
 #include <stdlib.h>
 
@@ -49,7 +48,6 @@ void obj_init(struct class_obj *obj, int x, int y, int dir, char obj_shape) {
 }
 
 void eneymies_init(struct class_obj obj[], int x, int y, int dir, char obj_shape) {
-    // obj = malloc(5 * sizeof(struct class_obj));
     for (int i = 0; i < 5; ++i) {
         obj[i].x = x;
         obj[i].y = y;
@@ -66,11 +64,11 @@ int main() {
     pthread_t thread_1, thread_2;
     struct application_settings arg;
 
-    obj_init(&player, 5, 5, 0, '@');
+    obj_init(&player, 5, 5, 0, 'D');
     // Bullet
     obj_init(&bullet, player.x, player.x, 0, '*');
 
-    eneymies_init(enemies, 50, 6, 0, '<');
+    eneymies_init(enemies, 50, 6, 0, 'O');
 
     initscr();
     clear();
@@ -83,7 +81,7 @@ int main() {
     win = newwin(HEIGHT, WIDTH, starty, startx);
     keypad(win, TRUE);
     refresh();
-    display_screen(win, Y, X, player);
+    // display_screen(win, Y, X, player);
 
     if (pthread_create(&thread_1, NULL, &display_enemies, win) != 0) {
         perror("Failed to create thread");
@@ -92,7 +90,9 @@ int main() {
 
     while (1) {
         c = wgetch(win);
-        mvwdelch(win, Y, X);
+        //mvwdelch(win, Y, X);
+        mvwaddch(win, Y, X, ' ');
+        wrefresh(win);
         switch (c) {
             case KEY_UP:
                 --Y;
@@ -106,7 +106,7 @@ int main() {
             case KEY_RIGHT:
                 ++X;
                 break;
-            case 's':
+            case 's': {
                 arg.win = win;
                 arg.X = X;
                 arg.Y = Y;
@@ -115,17 +115,36 @@ int main() {
                     perror("Failed to create thread");
                 }
                 break;
+            }
             default:
                 refresh();
                 break;
         }
-        display_screen(win, Y, X, player);
-        if (choice != 0)    /* User did a choice come out of the infinite loop */
-            break;
+        mvwaddch(win, Y, X, player.symbol);
+        wrefresh(win);
     }
     clrtoeol();
     refresh();
     endwin();
+    return 0;
+}
+
+void *display_bullet(void *arg) {
+    struct application_settings *app_settings;
+    app_settings = arg;
+
+    struct timespec ts;
+    uint16_t delay = 20;
+    ts.tv_sec = 0;
+    ts.tv_nsec = delay * 650000000;
+    int y = app_settings->Y;
+    int x = app_settings->X;
+    for (int i = x + 2; i < 35; i++) {
+        mvwaddch(app_settings->win, y, i, app_settings->ob.symbol);
+        wrefresh(app_settings->win);
+        nanosleep(&ts, NULL);
+        mvwaddch(app_settings->win, y, i, ' ');
+    }
     return 0;
 }
 
@@ -153,41 +172,21 @@ void *display_enemies(void *arg) {
         }
         nanosleep(&ts, NULL);
         for (int i = 0; i < 5; ++i) {
-            mvwdelch(win_en, enemies[i].y, enemies[i].x);
+            mvwaddch(win_en, enemies[i].y, enemies[i].x, ' ');
+            //  mvwdelch(win_en, enemies[i].y, enemies[i].x);
             wrefresh(win_en);
         }
         // mvwdelch(win_en, enemies[i].y, enemies[i].x);
-        //wclear(win_en);
-        //wrefresh(win_en);
+//        wclear(win_en);
+//        wrefresh(win_en);
     }
 }
 
-void *display_bullet(void *arg) {
-    struct application_settings *app_settings;
-    app_settings = arg;
-
-    struct timespec ts;
-    uint16_t delay = 20;
-    ts.tv_sec = 0;
-    ts.tv_nsec = delay * 10000000;
-    int y = app_settings->Y;
-
-    for (int i = app_settings->X + 2; i < 35;) {
-        i++;
-        nanosleep(&ts, NULL);
-        mvwaddch(app_settings->win, y, i, app_settings->ob.symbol);
-        wrefresh(app_settings->win);
-    }
-    refresh();
-    clear();
-}
-
-
-void display_screen(WINDOW *win, int Y, int X, struct class_obj ob) {
-//    refresh();
-//    clear();
-    //  mvwprintw(win, Y, X, ob.symbol);
-
-    mvwaddch(win, Y, X, ob.symbol);
-    wrefresh(win);
-}
+//void display_screen(WINDOW *win, int Y, int X, struct class_obj ob) {
+////    refresh();
+////    clear();
+//    //  mvwprintw(win, Y, X, ob.symbolz);
+//
+//    mvwaddch(win, Y, X, ob.symbol);
+//    wrefresh(win);
+//}
